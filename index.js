300bb3aececb1fe44d4b1737d7b9da4d85b6675e
Giuliana9212 <112766969+Giuliana9212@users.noreply.github.com>
2022-09-03 19:48:11 -0300

    link con js
    linkee el index.html con el index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const madre = parseInt(prompt("Ingrese la edad de su madre"));
+const padre = parseInt(prompt ("Ingrese la edad de su padre"));
+const hermano = parseInt(prompt ("Ingrese la edad de su hermano"));
+const hermana = parseInt(prompt ("Ingrese la edad de su hermana"));
+const ud = parseInt(prompt("Ingrese su edad"));
+
+const promedio = (madre + padre + hermano + hermana + ud)/5;
+console.log("El promedio de edad es", promedio);
+console.log("La edad es", madre + 7);
+console.log("La edad es", padre + 7);
+console.log("La edad es", hermano + 7);
+console.log("La edad es", hermana +7);
+console.log("La edad es", ud + 7);
